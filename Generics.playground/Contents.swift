c452bc2140340b9693a5fef8410c670584ff552e
RShankar <ravishankar@outlook.in>
2015-06-08 20:37:37 +0530

    Updated Generics
    Updated Generics

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,21 +1,11 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
-
 
 let numbers = [23,45,56,78,98]
 
 // reverse array with numbers
 
 func reverseNumber(items: Array<Int>) -> Array<Int> {
-    
-    var temp = Array<Int>()
-    
-    for item in reverse(items) {
-        temp.append(item)
-    }
-    
-    return temp
+    return items.reverse()
 }
 
 reverseNumber(numbers)
@@ -27,45 +17,22 @@ let strTemp = ["Deepak","John","Steve","Ravi","Ganesh"]
 func reverseString(items: Array<String>) -> Array<String> {
     
     var temp = Array<String>()
-    
-    for item in reverse(items) {
-        temp.append(item)
-    }
-    
-    return temp
+    return items.reverse()
 }
 
 reverseString(strTemp)
 
-/*
-
 func reverseItems<T>(items:[T])-> [T] {
-    
-    var temp = Array<T>()
-    
-    for item in reverse(items) {
-        temp.append(item)
-    }
-    
-    return temp
+    return items.reverse()
 }
 
 reverseItems(strTemp)
 reverseItems(numbers)
 
-*/
-
 class ReverseDemo<P> {
     
     func reverseItems(items:[P])-> [P] {
-        
-        var temp = Array<P>()
-        
-        for item in reverse(items) {
-            temp.append(item)
-        }
-        
-        return temp
+        return items.reverse()
     }
 }
 

a471187b806f64270fbd765c4beec9c87398e3e7
RShankar <ravi@rshankar.com>
2015-12-11 19:57:46 +0530

    maintenance
    maintenance

--- MemoryManagement.playground/Contents.swift
+++ MemoryManagement.playground/Contents.swift
@@ -2,10 +2,9 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
 // Memory Management
 
+/*
 class   Teacher   {
     var  name :   String?
     var  course :   String?
@@ -13,27 +12,27 @@ class   Teacher   {
     init   ( name :   String ,  course :   String )   {
         self . name  =  name
         self . course  =  course
-        println( "Reference count increased by 1" )
+        print( "Reference count increased by 1" )
     }
     
     deinit {
-        println( "Reference count decreased by 1" )
+        print( "Reference count decreased by 1" )
     }
 }
 
 let  teacher1  =   Teacher( name :   "Ravi" ,  course :   "Swift" )
 
 func   createTeacher( )   {
-    let  teacher2  =   Teacher( name :   "John" ,  course :   "Java" )
+    _  =   Teacher( name :   "John" ,  course :   "Java" )
 }
 
 createTeacher( )
+*/
 
 
-/* weak refrence */
+/* weak reference */
 
 /*
-
 class Teacher {
     var name:String?
     var course:String?
@@ -43,26 +42,26 @@ class Teacher {
         self.name = name
         self.course = course
         
-        println("Reference count of Teacher increases by 1")
+        print("Reference count of Teacher increases by 1")
     }
     
     deinit {
-        println("Reference count of Teacher decreases by 1")
+        print("Reference count of Teacher decreases by 1")
     }
 }
 
 class Student {
     var name:String?
-    var mentor: Teacher?
+    weak var mentor: Teacher?
     
     init(name: String, course:String) {
         self.name = name
         
-        println("Reference count of Student increases by 1")
+        print("Reference count of Student increases by 1")
     }
     
     deinit {
-        println("Reference count of Student decreases by 1")
+        print("Reference count of Student decreases by 1")
     }
 }
 
@@ -81,45 +80,43 @@ createInstance()
 /* unowned reference */
 
 /*
-
 class Teacher {
-var name:String?
-var course:String?
-var student: Student?
+    var name:String?
+    var course:String?
+    var student: Student?
 
-init(name: String, course:String) {
-self.name = name
-self.course = course
-println("Reference count of Teacher increases by 1")
-}
+    init(name: String, course:String) {
+        self.name = name
+        self.course = course
+        print("Reference count of Teacher increases by 1")
+    }
 
-deinit {
-println("Reference count of Teacher decreases by 1")
-}
+    deinit {
+        print("Reference count of Teacher decreases by 1")
+    }
 }
 
 class Student {
-var name:String?
-unowned var mentor: Teacher
+    var name:String?
+    unowned var mentor: Teacher
 
-init(name: String, course:String,mentor:Teacher) {
-self.name = name
-self.mentor = mentor
-println("Reference count of Student increases by 1")
-}
+    init(name: String, course:String,mentor:Teacher) {
+        self.name = name
+        self.mentor = mentor
+        print("Reference count of Student increases by 1")
+    }
 
-deinit {
-println("Reference count of Student decreases by 1")
-}
+    deinit {
+        print("Reference count of Student decreases by 1")
+    }
 }
 
 
 func createInstance() {
-
-let teacher = Teacher(name: "Jason", course: "Swift")
-let student = Student(name: "Adam", course: "Swift",mentor:teacher)
-teacher.student = student
-student.mentor = teacher
+    let teacher = Teacher(name: "Jason", course: "Swift")
+    let student = Student(name: "Adam", course: "Swift",mentor:teacher)
+    teacher.student = student
+    student.mentor = teacher
 }
 
 createInstance()
